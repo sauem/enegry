9c198f45b34bcbc480653b35e4160f8f7b912bdb
sauem <nguyendinhthang.go97@gmail.com>
2020-08-03 22:35:51 +0700

    code

--- wp-content/themes/tuan/header.php
+++ wp-content/themes/tuan/header.php
@@ -1,18 +1,5 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since Twenty Seventeen 1.0
- * @version 1.0
- */
-
-?><!DOCTYPE html>
+<?php ?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
